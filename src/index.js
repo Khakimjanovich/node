f2d7caa9075c04035ee073c4fc82b29b010b7f5c
Yunusali <abduraxmanovyunusali27@gmail.com>
2020-12-22 09:36:13 +0500

    git ignored

--- src/index.js
+++ src/index.js
@@ -6,16 +6,18 @@ const port = process.env.PORT_FOR_ECOMMERCE || 3003;
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
+
 app.use('/files', express.static("files"));
 app.use(morgan('dev'));
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Arise MERN developers'
     });
 });
 
-require("./config/mongoose.js")(app);
 require('./app/routerHandler')(app)
+require("./config/mongoose.js")(app);
 
 app.listen(port, () => {
     console.log(`Application is running on ${port}`);
